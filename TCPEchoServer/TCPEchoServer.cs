65057b46b59c1b28a6c57e86adb7211fcb0479ca
chmodder <hotmailgateway@gmail.com>
2017-02-19 19:36:09 +0100

    Added loop so that the server doesn't quit when client disconnects, although multiple clients cannot connect simultaniously, a client can connect after the the previous client disconnects from the server

--- TCPEchoServer/TCPEchoServer.cs
+++ TCPEchoServer/TCPEchoServer.cs
@@ -27,36 +27,42 @@ namespace TCPEchoServer
             TcpListener serverSocket = new TcpListener(6789);
             serverSocket.Start();
             Console.WriteLine($"Server Started");
-            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
-            //Socket connectionSocket = serverSocket.AcceptSocket();
-            Console.WriteLine("Server activated");
 
-            Stream ns = connectionSocket.GetStream();
-           // Stream ns = new NetworkStream(connectionSocket);
-
-            StreamReader sr = new StreamReader(ns);
-            StreamWriter sw = new StreamWriter(ns);
-            sw.AutoFlush = true; // enable automatic flushing
-
-            sw.WriteLine("Connection established");
-
-            string message = sr.ReadLine();
-            string answer = "";
-
-
-            while (message != null && message != "")
+            while (true)
             {
-                Console.WriteLine("Client: " + message);
-                answer = message.ToUpper();
-                sw.WriteLine(answer);
-                message = sr.ReadLine();
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                //Socket connectionSocket = serverSocket.AcceptSocket();
+                Console.WriteLine("Server activated");
+
+                Stream ns = connectionSocket.GetStream();
+                // Stream ns = new NetworkStream(connectionSocket);
+
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true; // enable automatic flushing
+
+                sw.WriteLine("Connection established");
+
+                string message = sr.ReadLine();
+                string answer = "";
+
+
+                while (message != null && message != "")
+                {
+                    Console.WriteLine("Client: " + message);
+                    answer = message.ToUpper();
+                    sw.WriteLine(answer);
+                    message = sr.ReadLine();
+                }
+
+                ns.Close();
+                connectionSocket.Close();
+
             }
 
-            ns.Close();
-            connectionSocket.Close();
             serverSocket.Stop();
             Console.ReadKey();
         }
     }
-    
+
 }
